b734eb45401f323f633b0cb5561262ffaa60f527
Dane Worsham <zenki14@gmail.com>
2015-01-13 22:40:03 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,40 @@
+var name;
+// Q: What does name equal right now?
+
+
+name = "Tim";
+// Q: What is `name` set to right now?
+
+
+name = "Bill";
+// Q: What is `name` set to right now?
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+
+
+math = high - "5";
+// Q: What is `math` set to?
+
+
+// Create a variable to calculate my age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+
+
+
+// Store some information following in variables.
+var yourname;
+var myname;
+
+var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
