ab9f7201a62737d00f288e65c195688ce97f0c9f
Dane Worsham <zenki14@gmail.com>
2015-01-13 23:12:39 -0500

    js intro finish

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 var name;
 // Q: What does name equal right now?
-
+// A: ""
 
 name = "Tim";
 // Q: What is `name` set to right now?
-
+// A: "Tim"
 
 name = "Bill";
 // Q: What is `name` set to right now?
-
+// A: "Bill"
 
 var greeting = "Hello, how are you?";
 // Q: What is `greeting` set to?
-
+// A: "Hello, how are you?"
 
 var high = 50;
 var low  = 10;
@@ -20,21 +20,28 @@ var low  = 10;
 
 var math = high - low;
 // Q: What is `math` set to?
-
+// A: 40
 
 math = high - "5";
 // Q: What is `math` set to?
-
+// A: 45
 
 // Create a variable to calculate my age
 // The answer should read "Tim is 31 years old"
 var born = 1983;
 var today = 2015;
-
+// A: name = 'Tim'
+// 		math = today - born
+// 		var statement = var statement = person + " is " +math + " years old.";
 
 
 // Store some information following in variables.
 var yourname;
 var myname;
-
 var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
+
+// A: 
+// var yourname = 'Dane'
+// var myname = 'Tim'
+// var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
+// statement
